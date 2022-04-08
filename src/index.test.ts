b3c9beb8bbe7f65963549645e2c67f4196ff7f6e
daidodo <daidodo@gmail.com>
2022-04-08 21:46:52 +0100

    Add impl and tests.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,36 @@
-import a from './';
+import findUp from '@dozerg/find-up';
 
-describe('Suite', () => {
-  it('should pass the test', () => {
-    expect(a).toBe(1);
+import requireModule from './';
+
+describe('requireModule', () => {
+  const moduleName = '@dozerg/find-up';
+  const fromPath = __dirname;
+  describe('Without default', () => {
+    describe('Found module', () => {
+      it('should return module', () => {
+        const result = requireModule(moduleName, fromPath);
+        expect(result).toBeDefined();
+      });
+    });
+    describe('Not found module', () => {
+      it('should return undefined', () => {
+        const result = requireModule('unknown-module-name', fromPath);
+        expect(result).toBeUndefined();
+      });
+    });
+  });
+  describe('With default', () => {
+    describe('Found module', () => {
+      it('should return module', () => {
+        const result = requireModule(moduleName, fromPath, 123).default;
+        expect(result).toBe(findUp);
+      });
+    });
+    describe('Not found module', () => {
+      it('should return default', () => {
+        const result = requireModule('unknown-module-name', fromPath, 123);
+        expect(result).toBe(123);
+      });
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
-/**
- * This is an example.
- */
-const a = 1;
+import log4js from 'log4js';
 
-export default a;
+import findUp from '@dozerg/find-up';
+
+export default function requireModule(moduleName: string, fromPath: string, defaultModule?: any) {
+  const log = logger('require-module.requireModule');
+  const userModule = requireUserModule(moduleName, fromPath);
+  if (userModule !== undefined) return userModule;
+  log.warn('Cannot find', moduleName, 'from path:', fromPath, 'and use default provided');
+  return defaultModule;
+}
+
+declare const __webpack_require__: typeof require;
+declare const __non_webpack_require__: typeof require;
+const req = typeof __webpack_require__ === 'function' ? __non_webpack_require__ : require;
+
+function requireUserModule(moduleName: string, fromPath: string) {
+  const log = logger('require-module.requireUserModule');
+  const [modulePath] = findUp.sync(`node_modules/${moduleName}`, {
+    cwd: fromPath,
+    stopAtLimit: 1,
+    type: 'directory',
+  });
+  if (!modulePath) return undefined;
+  log.debug('Found', moduleName, 'in', modulePath);
+  return req(modulePath);
+}
+
+function logger(category?: string) {
+  return log4js.getLogger(category);
+}
